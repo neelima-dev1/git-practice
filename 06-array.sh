cbca0867e596871caaeedfdcdb51e771abb31e26
neelima.t <neelima.t0510@gmail.com>
2024-11-04 15:51:47 +0530

    arraylist

---
+++ 06-array.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#index starts from 0, size is 3
+FRUITS=( "APPLE" "KIWI" "ORANGE") #ARRAY
+
+echo "First fruit is: ${FRUITS[0]}"
+
+echo "Second fruit is: ${FRUITS[1]}"
+
+echo "Third fruit is: ${FRUITS[2]}"
+
+echo "Fruits is: ${FRUITS[@]}"
