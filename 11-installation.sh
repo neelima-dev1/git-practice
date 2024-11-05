1471450ac12e04f86acec573baee080f05d14ac4
neelima.t <neelima.t0510@gmail.com>
2024-11-05 14:11:24 +0530

    functions

--- 11-installation.sh
+++ 11-installation.sh
@@ -15,6 +15,13 @@ if [ $? -ne 0 ]
 then
     echo "Git is not installed, going to install it.."
     dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "Git installation is not sucess .. check it"
+        exit 1
+    else
+        echo "Git installation is sucess"
+    fi        
 else
     echo "Git is already installed, nothing to ddo.."
 fi        
