5cee1e4ace2f9a9cff32115ddbb36271ec422b62
neelima.t <neelima.t0510@gmail.com>
2024-11-06 00:34:41 +0530

    functions

--- 12-functions.sh
+++ 12-functions.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 USERID=$(id -u)
-#echo "User id is: $USERID"
+
+CHECK_ROOT(){
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root privelages"
+    exit 1
+fi    
+
+}
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
@@ -12,12 +21,6 @@ VALIDATE(){
     fi    
 }
 
-if [ $USERID -ne 0 ]
-then
-    echo "Please run this script with root privelages"
-    exit 1
-
-fi
 dnf list installed  git 
 VALIDATE $?
 
