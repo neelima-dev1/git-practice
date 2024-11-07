3f3d69a54ba017520f84a07080d2ef66a516cd0d
neelima.t <neelima.t0510@gmail.com>
2024-11-07 19:04:41 +0530

    functions

---
+++ 16-redirectors.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/shell-script/"
+SCRIPT_NAME=$( echo $0 |cut -d "." -f1)
+TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
+mkdir -p $LOGS_FOLDER
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+y="\e[33m"
+N="\e[0m"
+CHECK_ROOT(){
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root privelages"
+    exit 1
+fi    
+}
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+    echo -e "$2 is .... $R FAILED $N"
+    exit
+    else
+        echo -e "$2 is .... $G SUCESS $N"
+    fi    
+}
+CHECK_ROOT
+# sh 15-loops.sh git mysql postfix nginx
+for package in $@ #$@ reffers to all arguments passed  to it
+do
+    dnf list installed $package
+    if [ $? -ne 0 ]
+    then
+        echo "$package is not installed, going to install it .."
+        dnf install $package -y
+        VALIDATE $? "Installing $package"
+    else
+        echo "$package is already installed .. nothing to do"
+    fi         
+done
