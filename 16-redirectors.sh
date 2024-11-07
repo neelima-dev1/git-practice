196031ee2202ca64c7838e2d1b6dc6609b393e98
neelima.t <neelima.t0510@gmail.com>
2024-11-07 19:10:27 +0530

    functions

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -15,30 +15,30 @@ CHECK_ROOT(){
 
 if [ $USERID -ne 0 ]
 then
-    echo "Please run this script with root privelages"
+    echo -e "$R Please run this script with root privelages $N" &>>$LOG_FILE
     exit 1
 fi    
 }
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-    echo -e "$2 is .... $R FAILED $N"
+    echo -e "$2 is .... $R FAILED $N" &>>$LOG_FILE
     exit
     else
-        echo -e "$2 is .... $G SUCESS $N"
+        echo -e "$2 is .... $G SUCESS $N" &>>$LOG_FILE
     fi    
 }
 CHECK_ROOT
-# sh 15-loops.sh git mysql postfix nginx
-for package in $@ #$@ reffers to all arguments passed  to it
-do
-    dnf list installed $package
-    if [ $? -ne 0 ]
-    then
-        echo "$package is not installed, going to install it .."
-        dnf install $package -y
-        VALIDATE $? "Installing $package"
-    else
-        echo "$package is already installed .. nothing to do"
-    fi         
-done
+#  sh 15-loops.sh git mysql postfix nginx
+# for package in $@ #$@ reffers to all arguments passed  to it
+# do
+#     dnf list installed $package
+#     if [ $? -ne 0 ]
+#     then
+#         echo "$package is not installed, going to install it .."
+#         dnf install $package -y
+#         VALIDATE $? "Installing $package"
+#     else
+#         echo "$package is already installed .. nothing to do"
+#     fi         
+# done
