2a26c29b08aebe51eb387ea93d574573db7ef779
neelima.t <neelima.t0510@gmail.com>
2024-11-09 19:33:04 +0530

    functions

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -35,7 +35,7 @@ USAGE(){
     exit 1
 }
 
-echo "Script Strated executing at: $(date)" &>>$LOG_FILE | tee -a $LOG_FILE
+echo "Script Strated executing at: $(date)" | tee -a $LOG_FILE
 
 CHECK_ROOT
 
