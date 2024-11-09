f5a32feb3a11ffac887f5c0f785378936ce13eb3
neelima.t <neelima.t0510@gmail.com>
2024-11-09 19:28:58 +0530

    functions

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -15,7 +15,7 @@ CHECK_ROOT(){
 
 if [ $USERID -ne 0 ]
 then
-    echo -e "$R Please run this script with root privelages $N" &>>$LOG_FILE
+    echo -e "$R Please run this script with root privelages $N" | tee -a &>>$LOG_FILE
     exit 1
 fi    
 }
@@ -23,10 +23,10 @@ fi
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-    echo -e "$2 is .... $R FAILED $N" &>>$LOG_FILE
+    echo -e "$2 is .... $R FAILED $N" &>>$LOG_FILE | tee -a &>>$LOG_FILE
     exit
     else
-        echo -e "$2 is .... $G SUCESS $N" &>>$LOG_FILE
+        echo -e "$2 is .... $G SUCESS $N" &>>$LOG_FILE | tee -a &>>$LOG_FILE
     fi    
 }
 
@@ -35,7 +35,7 @@ USAGE(){
     exit 1
 }
 
-echo "Script Strated executing at: $(date)" &>>$LOG_FILE
+echo "Script Strated executing at: $(date)" &>>$LOG_FILE | tee -a &>>$LOG_FILE
 
 CHECK_ROOT
 
@@ -47,13 +47,13 @@ fi
 #  sh 15-loops.sh git mysql postfix nginx
 for package in $@ #$@ reffers to all arguments passed  to it
 do
-    dnf list installed $package &>>$LOG_FILE
+    dnf list installed $package &>>$LOG_FILE 
     if [ $? -ne 0 ]
     then
-        echo "$package is not installed, going to install it .."
+        echo "$package is not installed, going to install it .." | tee -a &>>$LOG_FILE
         dnf install $package -y &>>$LOG_FILE
         VALIDATE $? "Installing $package"
     else
-        echo -e "$package is already $Y installed .. nothing to do $N" &>>$LOG_FILE
+        echo -e "$package is already $Y installed .. nothing to do $N" | tee -a &>>$LOG_FILE
     fi         
 done
