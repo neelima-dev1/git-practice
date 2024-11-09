7395fe7656597f5b543e4a27564f99d99a13add3
neelima.t <neelima.t0510@gmail.com>
2024-11-09 18:38:14 +0530

    functions

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -15,7 +15,7 @@ CHECK_ROOT(){
 
 if [ $USERID -ne 0 ]
 then
-    echo -e " $R Please run this script with root privelages $N" &>>$LOG_FILE
+    echo -e "$R Please run this script with root privelages $N" &>>$LOG_FILE
     exit 1
 fi    
 }
