69e89ea7ed4100e25f5fe3c0ba0488c3cb356e18
neelima.t <neelima.t0510@gmail.com>
2024-12-02 15:16:00 +0530

    shell-script

--- 17-set.sh
+++ 17-set.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e # setting the automatic exit, if we get error
+set -ex # setting the automatic exit, if we get error
 echo "Hello World Sucess"
 echoooo "Hello World Failure"
 echo "Hello World after failure"
