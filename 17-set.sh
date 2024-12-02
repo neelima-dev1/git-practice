fc5aefd6967517c5fd20471a81ec83fd133610e2
neelima.t <neelima.t0510@gmail.com>
2024-12-02 15:26:08 +0530

    shell-script

--- 17-set.sh
+++ 17-set.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-set -ex # setting the automatic exit, if we get error
+set -ex # setting the automatic exit, if we get error, set -ex for debug
+failure(){
+    echo "Failed at: $1:$2"
+}
+trap 'failure "${LINENO}" "$BASH_COMMAND"' ERR # ERR is the error signal 
 echo "Hello World Sucess"
 echoooo "Hello World Failure"
 echo "Hello World after failure"
