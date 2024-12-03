e013b164548de80fafb03b415d94666232f19b5a
neelima.t <neelima.t0510@gmail.com>
2024-12-03 16:43:46 +0530

    shell-script

--- 19-backup.sh
+++ 19-backup.sh
@@ -3,6 +3,7 @@
 SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3:-14} # IF $3 is empty, default is 14 days
+TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
 
 R="\e[31m"
 G="\e[32m"
@@ -37,9 +38,28 @@ FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 
 echo "Files: $FILES"
 
-if [ ! -z $FILES ] #true if FILES are empty
+if [ ! -z $FILES ] #true if FILES are empty, ! makes it expression false
 then
     echo "Files are found"
+    ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
+    find $SOURCE_DIR -name "*.log" -mtime +14 |zip "$ZIP_FILE" -@
+    #CHECK IF zip file is sucessfully created or not
+    if [ -f $ZIP_FILE ]
+    then
+        echo "Sucessfully zipped files older than $DAYS"
+
+        #remove the files after zipping
+        
+         while IFS= read -r file #IFS is called internal field separator, empty it will not ignore while space. -r is for not to ignore special characters like /
+         do
+             echo "Deleting file: $file"
+             rm -rf $file
+         done <<< $FILES
+
+    else 
+        echo "Zipping the files is failed"
+        exit 1
+    fi
 else
     echo "No Files older than $DAYS"
 fi
