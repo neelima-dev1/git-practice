ae993829605dca17bc1214c84565f557efae54ce
neelima.t <neelima.t0510@gmail.com>
2024-12-03 18:04:57 +0530

    shell-script

--- 20-disk-usage.sh
+++ 20-disk-usage.sh
@@ -7,7 +7,7 @@ DISK_THRESOLD=5 #Real projects, it is usally 75
 
 while IFS= read -r line #IFS is called internal field separator, empty it will not ignore while space. -r is for not to ignore special characters like /
 do
-    USAGE=$(echo $line) #df -HT | grep xfs | awk -F " " '{print $6F}'
-
+    #USAGE=$(echo $line) #df -HT | grep xfs | awk -F " " '{print $6F}'
+    echo $line
     
 done <<< $DISK_USAGE
